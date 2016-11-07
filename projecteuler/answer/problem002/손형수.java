a4774590e38e6d02fa8a77295dadc088b1d790c7
sgshs <sgshs@mgaia_sgshs>
2016-11-07 19:56:06 +0900

    Signed-off-by: sgshs <sgshs@mgaia_sgshs>

---
+++ projecteuler/answer/problem002/손형수.java
@@ -0,0 +1,19 @@
+package answer.problem002;
+
+
+class 손형수{
+	
+	final int firstNum = 1;		// 피보나치 첫번째수
+	final int secondNum = 2;	// 피보나치 두번째수
+	
+	final int limit = 4000000;	// limit
+	
+	
+	public static void main(String[] args){
+		int result = 0;			// 합계 계산 변수
+		
+		System.out.println("정답은 " + result + " 입니다.");
+	}
+
+	
+}
